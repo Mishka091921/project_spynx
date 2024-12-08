d2821ad1b98dd2dbfc1fd4e7d8a5b1f033b2ac29
joshua <joshuagementiza.groundlink@gmail.com>
2024-12-08 16:54:45 +0800

    test

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -20,7 +20,7 @@ const Pricing = () => {
             <div className="p-10 border text-center border-neutral-700 rounded-xl">
               TOTAL SUPPLY:
               <br/>
-              1,000,000,000
+              750,000,000
             </div>
           </div>
           <div className="w-full sm:w-1/2 lg:w-1/4 p-2">
@@ -34,7 +34,7 @@ const Pricing = () => {
             <div className="p-10 border text-center border-neutral-700 rounded-xl">
               TOTAL MINTS:
               <br/>
-              40,00
+              30,00
             </div>
           </div>
       </div>
--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -8,7 +8,7 @@ const Workflow = () => {
   return (
     <div className="mt-20">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
-        Roadmap{" "}
+        What's More?{" "}
         
       </h2>
       <div className="flex flex-wrap justify-center">
